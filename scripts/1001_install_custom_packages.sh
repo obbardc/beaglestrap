b775301a792146174aff2330f1970d90ca01d30d
Christopher Obbard <obbardc@gmail.com>
2017-12-29 17:52:23 +0000

    more automation

--- scripts/1001_install_custom_packages.sh
+++ scripts/1001_install_custom_packages.sh
@@ -18,6 +18,7 @@ chroot_exec usermod -p "${ENCRYPTED_PASSWORD}" $USERNAME
 # copy in soundcard settings
 cp /home/chris/projects/oldenburg/bbb_audio_extension.alsactl.state $ROOTFS/home/jack/bbb_audio_extension.alsactl.state
 cp /home/chris/projects/oldenburg/bbb_audio_extension.alsactl.state $ROOTFS/root/bbb_audio_extension.alsactl.state
+chroot_exec chown jack:jack /home/jack/bbb_audio_extension.alsactl.state
 
 # copy in compiled jack
 cp /home/chris/projects/oldenburg/jackd2/*.deb $ROOTFS/tmp/
@@ -28,10 +29,10 @@ echo "jackd2	jackd/tweak_rt_limits	boolean	true" > $ROOTFS/tmp/debconf_selection
 chroot_exec debconf-set-selections /tmp/debconf_selections.conf
 rm $ROOTFS/tmp/debconf_selections.conf
 chroot_exec dpkg -i /tmp/libjack-jackd2-0_1.9.10+20150825git1ed50c92~dfsg-5_armhf.deb
-chroot_exec apt-get install -f
+chroot_exec apt-get install -f --yes
 chroot_exec dpkg -i /tmp/libjack-jackd2-dev_1.9.10+20150825git1ed50c92~dfsg-5_armhf.deb
-chroot_exec apt-get install -f
+chroot_exec apt-get install -f --yes
 chroot_exec dpkg -i /tmp/jackd2_1.9.10+20150825git1ed50c92~dfsg-5_armhf.deb
 rm $ROOTFS/tmp/*.deb
-chroot_exec apt-get install -f
+chroot_exec apt-get install -f --yes
 chroot_exec apt-get clean
