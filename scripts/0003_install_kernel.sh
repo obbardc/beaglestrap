72b5f1c88fe1c87233ddcc1cf42335c7062ea138
Christopher Obbard <obbardc@gmail.com>
2017-12-30 14:34:44 +0000

    change dts name

--- scripts/0003_install_kernel.sh
+++ scripts/0003_install_kernel.sh
@@ -15,7 +15,7 @@ cp $UBOOT_DIR/u-boot.img $ROOTFS/boot/
 
 # copy kernel & dtb
 cp $LINUX_DIR/arch/arm/boot/zImage $ROOTFS/boot/
-cp $LINUX_DIR/arch/arm/boot/dts/am335x-boneblack-soundcard.dtb $ROOTFS/boot/
+cp $LINUX_DIR/arch/arm/boot/dts/am335x-boneblack-audio-extension.dtb $ROOTFS/boot/
 
 # copy kernel modues
 cp $LINUX_DIR/compiled_modules/* ${ROOTFS}/ -R
@@ -30,13 +30,12 @@ fdtaddr=0x88000000
 rdaddr=0x88080000
 
 load mmc 0:1 ${loadaddr} zImage
-load mmc 0:1 ${fdtaddr} am335x-boneblack-soundcard.dtb
+load mmc 0:1 ${fdtaddr} am335x-boneblack-audio-extension.dtb
 
 setenv bootargs console=ttyO0,115200n8 noinitrd root=/dev/mmcblk0p2 rootfstype=ext4 rw rootwait
 bootz ${loadaddr} - ${fdtaddr}
 EOF
 
-# build it anyway even though we are not using
 ${UBOOT_DIR}/tools/mkimage -C none -A arm -T script -d $ROOTFS/boot/boot.cmd $ROOTFS/boot/boot.scr
 
 cp $ROOTFS/boot/boot.cmd $ROOTFS/boot/uEnv.txt
