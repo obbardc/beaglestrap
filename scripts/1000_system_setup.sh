86a567a3601df760fb4bc013ce2304dc78c4f1af
Christopher Obbard <chris@64studio.com>
2018-04-05 20:02:49 +0100

    update ssh keys on first boot

--- scripts/1000_system_setup.sh
+++ scripts/1000_system_setup.sh
@@ -51,3 +51,27 @@ chroot_exec apt-get install openssh-server --yes
 
 # allow root logins (this is temporary, do not worry)
 sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' $ROOTFS/etc/ssh/sshd_config
+
+
+# startup script to generate new ssh host keys
+rm -f $ROOTFS/etc/ssh/ssh_host_*
+cat << EOF > $ROOTFS/etc/init.d/ssh_gen_host_keys
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          Generates new ssh host keys on first boot
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:
+# Short-Description: Generates new ssh host keys on first boot
+# Description:       Generates new ssh host keys on first boot
+### END INIT INFO
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -t rsa -N ""
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -t dsa -N ""
+insserv -r /etc/init.d/ssh_gen_host_keys
+service ssh start
+update-rc.d ssh defaults
+rm -f \$0
+EOF
+chmod a+x $ROOTFS/etc/init.d/ssh_gen_host_keys
+insserv $ROOTFS/etc/init.d/ssh_gen_host_keys
